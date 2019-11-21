ba29aa26dbd8c28108001d50dfbc51789c32eb32
ayoussefinia <alex.youssefinia@gmail.com>
2019-11-21 16:10:17 -0600

    commit changes to be able to run eject script for css scoping

---
+++ client/src/components/Nav/Nav.js
@@ -0,0 +1,17 @@
+import React from "react";
+import classes from "./Nav.css"
+
+function Nav() {
+  return (
+    // <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+    //   <a className="navbar-brand" href="/">
+    //     React Reading List
+    //   </a>
+    // </nav>
+    <div className= {classes.navbar}>
+      hello
+    </div>
+  );
+}
+
+export default Nav;
