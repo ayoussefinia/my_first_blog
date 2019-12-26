4b1ae132aca3fcaee6ad92a0ba649fbe60a3c9d2
ayoussefinia <alex.youssefinia@gmail.com>
2019-12-25 18:25:54 -0600

    authentication is a pain in the *** merry christmas

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: "mongodb://alex.youssefinia%40gmail.com:Orange1%40@ds019876.mlab.com:19876/heroku_8ksdw6hl" ,
+  secretOrKey: "secret"
+};
--- server.js
+++ server.js
@@ -1,22 +1,54 @@
 const express = require("express");
 
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 const routes = require("./routes");
+
+
+const users = require("./routes/api/users");
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-// Add routes, both API and view
-app.use(routes);
+
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/communityblog");
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/communityblog");
+
+const db = require("./config/keys").mongoURI;
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
+  
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require("./config/passport")(passport);
+
+
+// Routes
+app.use("/api/users", users);
+
+// Add routes, both API and view
+// app.use(routes);
 
 // Start the API server
 app.listen(PORT, function() {
